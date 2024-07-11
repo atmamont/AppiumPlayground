47024cf196e393fd22a74dfe24f007dd7daac84d
Andrii Mamchenko <andrii.mamchenko@adyen.com>
2024-07-11 17:02:13 +0200

    Remove files

--- test/spec/cartSpec.js
+++
@@ -1,7 +0,0 @@
-describe('Add item to the cart', () => {
-    it('Taps add to cart on the first item', async () => {
-        await $('~store-item-button-01').click();
-        const cartBadgeValue = await $('~Cart').getValue();
-        expect(cartBadgeValue).toHaveText('1 item');
-    })
-})
